c30f7e4d75c535ffa516684fbc98160d6d5e566e
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-14 12:15:24 -0400

    Repository module creado

---
+++ app/src/main/java/com/example/techstore/di/RepositoryModule.kt
@@ -0,0 +1,50 @@
+package com.example.techstore.di
+
+import com.example.techstore.data.repository.*
+import com.example.techstore.domain.repository.*
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindProductoRepository(
+        productoRepositoryImpl: ProductoRepositoryImpl
+    ): ProductoRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCartRepository(
+        cartRepositoryImpl: CartRepositoryImpl
+    ): CartRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCategoryRepository(
+        categoryRepositoryImpl: CategoryRepositoryImpl
+    ): CategoryRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindFavoriteRepository(
+        favoriteRepositoryImpl: FavoriteRepositoryImpl
+    ): FavoriteRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindOrderRepository(
+        orderRepositoryImpl: OrderRepositoryImpl
+    ): OrderRepository
+}
