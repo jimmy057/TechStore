4f8e4b2e3e9056f3dc5427783b02958471269dff
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-14 11:32:12 -0400

    entidad de pedido creada

---
+++ app/src/main/java/com/example/techstore/data/local/entities/PedidoEntity.kt
@@ -0,0 +1,15 @@
+package com.example.techstore.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "pedidos")
+data class PedidoEntity(
+    @PrimaryKey val id: Int,
+    val fecha: String,
+    val total: Double,
+    val estado: String?,
+    val direccionEnvio: String?,
+    val metodoPago: String?,
+    val usuarioId: Int
+)
