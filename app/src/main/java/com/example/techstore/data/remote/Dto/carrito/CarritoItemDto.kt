b27dd5b339b348753d6aebed50c10eebaf57a4cf
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-14 11:43:32 -0400

    Dto de CarritoItem creado

---
+++ app/src/main/java/com/example/techstore/data/remote/Dto/carrito/CarritoItemDto.kt
@@ -0,0 +1,11 @@
+package com.example.techstore.data.remote.Dto.carrito
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class CarritoItemDto(
+    @Json(name = "usuarioId") val usuarioId: Int,
+    @Json(name = "productoId") val productoId: Int,
+    @Json(name = "cantidad") val cantidad: Int
+)
