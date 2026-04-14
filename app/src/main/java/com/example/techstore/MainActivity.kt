591042e67d51aac286c9b37cb2e8422d0b709358
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-14 13:45:11 -0400

    agregando el sessionDatastore en el mainActivity para el navigation

--- app/src/main/java/com/example/techstore/MainActivity.kt
+++ app/src/main/java/com/example/techstore/MainActivity.kt
@@ -3,75 +3,33 @@ package com.example.techstore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.example.techstore.data.local.datastore.SessionDataStore
+import com.example.techstore.presentacion.Navegacion.AppNavHost
 import com.example.techstore.ui.theme.TechStoreTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var sessionDataStore: SessionDataStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TechStoreTheme {
-
-                val navController = rememberNavController()
-
-                NavHost(
-                    navController = navController,
-                    startDestination = "home"
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-
-                    composable("home") {
-                        HomeScreen(navController)
-                    }
-
-                    composable("productos") {
-                        ProductosScreen()
-                    }
+                    AppNavHost(sessionDataStore = sessionDataStore)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Text(text = "Hello $name!")
-
-        Button(
-            onClick = {
-                println("Botón presionado")
-            }
-        ) {
-            Text("Comprar ahora 🛒")
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TechStoreTheme {
-        Greeting("Android")
     }
 }
