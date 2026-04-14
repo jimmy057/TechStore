37593532694a31ea27272dc72b1c1907bcadd6db
jimmy057 <jimmypolanco2540@gmail.com>
2026-04-14 13:52:06 -0400

    DataStoreModule creado

---
+++ app/src/main/java/com/example/techstore/di/DataStoreModule.kt
@@ -0,0 +1,34 @@
+package com.example.techstore.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.datastore.preferences.preferencesDataStoreFile
+import com.example.techstore.data.local.datastore.SessionDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "session_prefs")
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return context.dataStore
+    }
+
+    @Provides
+    @Singleton
+    fun provideSessionDataStore(dataStore: DataStore<Preferences>): SessionDataStore {
+        return SessionDataStore(dataStore)
+    }
+}
